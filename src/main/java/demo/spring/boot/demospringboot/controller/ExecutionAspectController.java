801cae77cf059b5fdb67f3c00b5004ee16394c6a
chao <m18888106873@163.com>
2018-10-19 10:21:11 +0800

    添加ExecutionAspect

---
+++ src/main/java/demo/spring/boot/demospringboot/controller/ExecutionAspectController.java
@@ -0,0 +1,33 @@
+package demo.spring.boot.demospringboot.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import demo.spring.boot.demospringboot.framework.Code;
+import demo.spring.boot.demospringboot.framework.Response;
+
+/**
+ * 2018/10/18    Created by   chao
+ */
+@RestController
+@RequestMapping("/executionAspect")
+public class ExecutionAspectController {
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    /**
+     * 所有的异常处理在ExecutionAspect切面中执行
+     */
+    @GetMapping("/aspect")
+    public Response framework(@RequestParam(value = "one") Integer one,
+                              @RequestParam(value = "two") Integer two) {
+        Response response = new Response();
+        response.setCode(Code.System.OK);
+        response.setMsg(Code.System.SERVER_SUCCESS_MSG);
+        response.setContent(one / two);
+        return response;
+    }
+}
--- src/main/java/demo/spring/boot/demospringboot/framework/exception/catcher/ExceptionCatcher.java
+++ src/main/java/demo/spring/boot/demospringboot/framework/exception/catcher/ExceptionCatcher.java
@@ -1,7 +1,11 @@
 package demo.spring.boot.demospringboot.framework.exception.catcher;
 
+import demo.spring.boot.demospringboot.framework.Code;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import demo.spring.boot.demospringboot.framework.Response;
 
 /**
  * 全局捕获异常
@@ -9,8 +13,21 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class ExceptionCatcher {
 
-    @ExceptionHandler(value = NullPointerException.class)
-    public String NullPointException() {
-        return "空指针异常";
+    /**
+     * 用于捕获全局异常，Controller发生异常，如果没有处理，就会在这里统一处理
+     *
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(value = Exception.class)
+    @ResponseBody
+    public Response ExceptionDeal(Exception e) {
+        Response response = new Response();
+        response.setCode(Code.System.SYSTEM_ERROR_CODE);
+        response.setMsg(Code.System.SYSTEM_ERROR_CODE_MSG);
+        response.addException(e);
+        response.setError(e.getMessage());
+        return response;
+
     }
 }
